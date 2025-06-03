087d64d42eab6fd6060e61e4520372d72d1a0630
Goro7777 <gor.mainaddress@gmail.com>
2025-06-03 13:36:21 +0300

    Add open a single message

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const path = require("node:path");
 const express = require("express");
-const { getTextShort } = require("./util");
+const { getTextShort, uuid } = require("./util");
 
 const app = express();
 
@@ -11,11 +11,13 @@ app.set("view engine", "ejs");
 
 const messages = [
     {
+        id: uuid(),
         text: "Hi there!",
         user: "Amando",
         added: new Date(),
     },
     {
+        id: uuid(),
         text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
         user: "Charles",
         added: new Date(),
@@ -30,12 +32,19 @@ app.get("/", (req, res) => {
     res.render("pages/index", { messages: messagesShort });
 });
 
+app.get("/messages/:id", (req, res) => {
+    const id = req.params.id;
+    let message = messages.find((m) => m.id === id);
+    res.render("pages/message", { message });
+});
+
 app.get("/form", (req, res) => {
     res.render("pages/form");
 });
 
 app.post("/new", (req, res) => {
     let newMessage = {
+        id: uuid(),
         text: req.body.messageText,
         user: req.body.messageUser,
         added: new Date(),
--- util.js
+++ util.js
@@ -16,6 +16,14 @@ function getTextShort(textFull) {
     return textShort;
 }
 
+function uuid() {
+    return String(Date.now().toString(32) + Math.random().toString(16)).replace(
+        /\./g,
+        ""
+    );
+}
+
 module.exports = {
     getTextShort,
+    uuid,
 };
