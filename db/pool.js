38e17fac5e21cfb588d8323a57c206a6362af1dd
Goro7777 <gor.mainaddress@gmail.com>
2025-06-04 16:27:51 +0300

    Deploy with a database

--- db/pool.js
+++ db/pool.js
@@ -1,12 +1,23 @@
 const { Pool } = require("pg");
 
-// All of the following properties should be read from environment variables
-// We're hardcoding them here for simplicity
+// const pool = new Pool({
+//     host: "localhost", // or wherever the db is hosted
+//     user: process.env.USER,
+//     database: "messages_db",
+//     password: process.env.PASSWORD,
+//     port: 5432, // The default port
+// });
 
-module.exports = new Pool({
-    host: "localhost", // or wherever the db is hosted
-    user: process.env.USER,
-    database: "messages_db",
-    password: process.env.PASSWORD,
-    port: 5432, // The default port
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    // ssl: {
+    //     rejectUnauthorized: false,
+    // },
 });
+
+pool.connect((err) => {
+    if (err) throw err;
+    console.log("Connect to PostgreSQL successfully!");
+});
+
+module.exports = pool;
